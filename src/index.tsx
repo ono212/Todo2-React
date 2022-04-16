1ebcca9061b9ea47a694a9f603d5cfd9bbd298fa
jungmin <qkrwjdals302@naver.com>
2022-04-16 23:10:42 +0900

    index 파일 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom'; // /client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom"; // /client';
+import "./index.css";
+import App from "./App";
 
 /* 기존에 이렇게 작성되어져 있었는데 강의와 달라서 주석처리.
 const root = ReactDOM.createRoot(
@@ -15,4 +15,4 @@ root.render(
 */
 
 // App.tsx 파일에 설정된 html element를 id가 root인 element에 렌더링하라고 지시하는 코드
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
