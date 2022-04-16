388100566f0fd4af9802681a635056c92332ae16
jungmin <qkrwjdals302@naver.com>
2022-04-16 23:02:41 +0900

    ReactDOM.render 지원 중단에 의해 createRoot로 대체

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import Todo from "./components/Todo";
+
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo title="그릇 닦기" />
+      <Todo title="돈 보내주기" />
+      <Todo title="피카츄 배구하기" />
+    </div>
+  );
 }
 
 export default App;
